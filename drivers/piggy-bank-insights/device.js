1e83e86991ee80d518937db9922ad9622f88f120
frodeheg <frode.heggelund@gmail.com>
2022-07-24 02:34:11 +0200

    Added configurable poll interval for the device

--- drivers/piggy-bank-insights/device.js
+++ drivers/piggy-bank-insights/device.js
@@ -4,7 +4,7 @@
 
 const { Device } = require('homey');
 
-const POLL_INTERVAL = 1000 * 60 * 1; // Poll the app once every minute
+const DEFAULT_POLL_INTERVAL = 60; // Number of seconds to poll data
 
 class MyDevice extends Device {
 
@@ -15,10 +15,22 @@ class MyDevice extends Device {
     this.log('MyDevice has been initialized');
     this.intervalID = undefined;
 
-    // Every minute check if the state has changed:
-    this.intervalID = setInterval(() => {
-      this.updateState();
-    }, POLL_INTERVAL);
+    // Fetch poll interval and set up timer
+    const settings = this.getSettings();
+    this.setPollIntervalTime(settings['refreshRate']);
+    this.intervalID = setTimeout(() => this.updateState(), this.__pollIntervalTime);
+  }
+
+  /**
+   * Sets the poll interval time
+   * @param newTime is in seconds
+   */
+  setPollIntervalTime(newTime) {
+    let myTime = +newTime; // Convert to number in case it is not
+    if (typeof myTime !== 'number' || myTime < 5 || myTime > 60) {
+      myTime = DEFAULT_POLL_INTERVAL;
+    }
+    this.__pollIntervalTime = myTime * 1000; // Change from seconds to ms
   }
 
   /**
@@ -38,6 +50,10 @@ class MyDevice extends Device {
    */
   async onSettings({ oldSettings, newSettings, changedKeys }) {
     this.log('MyDevice settings where changed');
+    if ('refreshRate' in changedKeys) {
+      this.setPollIntervalTime(newSettings['refreshRate']);
+      // The new setting will be applied after next refresh
+    }
   }
 
   /**
@@ -66,48 +82,52 @@ class MyDevice extends Device {
    * Polls all the state from the app and updates the device
    */
   async updateState() {
-    const piggyState = this.homey.app.getState();
-    // this.log("Updating state: " + JSON.stringify(piggyState));
-    if (piggyState.power_last_hour !== undefined) {
-      this.setCapabilityValue('meter_power.last_hour', piggyState.power_last_hour);
-    }
-    if (piggyState.power_estimated !== undefined) {
-      this.setCapabilityValue('meter_power.estimated', piggyState.power_estimated);
-    }
-    this.setCapabilityValue('alarm_generic.overshoot', piggyState.alarm_overshoot);
-
-    this.setCapabilityValue('measure_power.free_capacity', Math.round(piggyState.free_capacity));
-    this.setCapabilityValue('measure_power.reserved_power', piggyState.safety_power);
-    let percentageOn = Math.round((100 * (piggyState.num_devices - piggyState.num_devices_off)) / piggyState.num_devices);
-    percentageOn = (percentageOn < 0) ? 0 : (percentageOn > 100) ? 100 : percentageOn;
-    this.setCapabilityValue('piggy_devices_on', percentageOn);
-
-    // Set Price point capability + update timeline using boolean workaround capabilities
-    const prevPricePoint = await this.getStoreValue('piggy_price');
-    this.setCapabilityValue('piggy_price', piggyState.price_point);
-    if (piggyState.price_point !== prevPricePoint) {
-      this.setStoreValue('piggy_price', piggyState.price_point);
-      switch (piggyState.price_point) {
-        case '0': this.toggleCapability('piggy_price_low'); break;
-        case '1': this.toggleCapability('piggy_price_normal'); break;
-        case '2': this.toggleCapability('piggy_price_expensive'); break;
-        default: /* Broken input should not happen */ break;
+    try {
+      const piggyState = this.homey.app.getState();
+      // this.log("Updating state: " + JSON.stringify(piggyState));
+      if (piggyState.power_last_hour !== undefined) {
+        this.setCapabilityValue('meter_power.last_hour', piggyState.power_last_hour);
       }
-    }
-
-    // Set Mode capability + update timeline using boolean workaround capabilities
-    const prevMode = await this.getStoreValue('piggy_mode');
-    this.setCapabilityValue('piggy_mode', piggyState.operating_mode);
-    if (piggyState.operating_mode !== prevMode) {
-      this.setStoreValue('piggy_mode', piggyState.operating_mode);
-      switch (piggyState.operating_mode) {
-        case '0': this.toggleCapability('piggy_mode_disabled'); break;
-        case '1': this.toggleCapability('piggy_mode_normal'); break;
-        case '2': this.toggleCapability('piggy_mode_night'); break;
-        case '3': this.toggleCapability('piggy_mode_holiday'); break;
-        case '4': this.toggleCapability('piggy_mode_custom'); break;
-        default: /* Broken input should not happen */ break;
+      if (piggyState.power_estimated !== undefined) {
+        this.setCapabilityValue('meter_power.estimated', piggyState.power_estimated);
       }
+      this.setCapabilityValue('alarm_generic.overshoot', piggyState.alarm_overshoot);
+
+      this.setCapabilityValue('measure_power.free_capacity', Math.round(piggyState.free_capacity));
+      this.setCapabilityValue('measure_power.reserved_power', piggyState.safety_power);
+      let percentageOn = Math.round((100 * (piggyState.num_devices - piggyState.num_devices_off)) / piggyState.num_devices);
+      percentageOn = (percentageOn < 0) ? 0 : (percentageOn > 100) ? 100 : percentageOn;
+      this.setCapabilityValue('piggy_devices_on', percentageOn);
+
+      // Set Price point capability + update timeline using boolean workaround capabilities
+      const prevPricePoint = await this.getStoreValue('piggy_price');
+      this.setCapabilityValue('piggy_price', piggyState.price_point);
+      if (piggyState.price_point !== prevPricePoint) {
+        this.setStoreValue('piggy_price', piggyState.price_point);
+        switch (piggyState.price_point) {
+          case '0': this.toggleCapability('piggy_price_low'); break;
+          case '1': this.toggleCapability('piggy_price_normal'); break;
+          case '2': this.toggleCapability('piggy_price_expensive'); break;
+          default: /* Broken input should not happen */ break;
+        }
+      }
+
+      // Set Mode capability + update timeline using boolean workaround capabilities
+      const prevMode = await this.getStoreValue('piggy_mode');
+      this.setCapabilityValue('piggy_mode', piggyState.operating_mode);
+      if (piggyState.operating_mode !== prevMode) {
+        this.setStoreValue('piggy_mode', piggyState.operating_mode);
+        switch (piggyState.operating_mode) {
+          case '0': this.toggleCapability('piggy_mode_disabled'); break;
+          case '1': this.toggleCapability('piggy_mode_normal'); break;
+          case '2': this.toggleCapability('piggy_mode_night'); break;
+          case '3': this.toggleCapability('piggy_mode_holiday'); break;
+          case '4': this.toggleCapability('piggy_mode_custom'); break;
+          default: /* Broken input should not happen */ break;
+        }
+      }
+    } finally {
+      this.intervalID = setTimeout(() => this.updateState(), this.__pollIntervalTime);
     }
   }
 
