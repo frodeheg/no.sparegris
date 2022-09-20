4581a40ede9d91fa0dffefbfef38c86e5ade1dbe
frodeheg <frode.heggelund@gmail.com>
2022-09-20 03:02:49 +0200

    Improved debugging scripts

--- debug_stop.sh
+++ debug_stop.sh
@@ -4,5 +4,5 @@ sed -i 's/"Sparegris 2"/"Sparegris"/g' .homeycompose/app.json
 sed -i 's/"no.sparegris2"/"no.sparegris"/g' ./app.json
 sed -i 's/"Sparegris 2"/"Sparegris"/g' ./app.json
 
-sed -i 's/DEBUG_ACTIVE_BEGIN \*\//DEBUG_BEGIN/g' ./app.js
-sed -i 's/\/\* DEBUG_ACTIVE_END/DEBUG_END/g' ./app.js
+sed -i 's/DEBUG_ACTIVE_BEGIN \*\//DEBUG_BEGIN/g' ./app.js ./settings/index.html
+sed -i 's/\/\* DEBUG_ACTIVE_END/DEBUG_END/g' ./app.js ./settings/index.html
