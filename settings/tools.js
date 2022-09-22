4013914e8bc309c9637b2419b0e5f25c6734a97d
frodeheg <frode.heggelund@gmail.com>
2022-09-22 10:06:17 +0200

    Made the functions to find average and max of elements resilliant to NaN and undefined

--- settings/tools.js
+++ settings/tools.js
@@ -6,17 +6,16 @@
  * @returns an array of indices to the 3 largest elements
  */
 function getMax3(ar) {
-  if (ar.length <= 3) return Object.keys(ar);
-  const max = [
-    { value: ar[0], index: 0 },
-    { value: ar[1], index: 1 },
-    { value: ar[2], index: 2 }];
-  max.sort((a, b) => a.value - b.value);
-  for (let i = 3; i < ar.length; i++) {
-    if (ar[i] > max[0].value) {
-      max[0] = { value: ar[i], index: i };
-      max.sort((a, b) => a.value - b.value);
+  const max = [];
+  for (let i = 0; i < ar.length; i++) {
+    if (Number.isNaN(+ar[i])) {
+      continue;
+    } else if (max.length < 3) {
+      max.push({ value: +ar[i], index: i });
+    } else if (+ar[i] > max[0].value) {
+      max[0] = { value: +ar[i], index: i };
     }
+    max.sort((a, b) => a.value - b.value);
   }
   return max.reduce((a, b) => (Array.isArray(a) ? [...a, b.index] : [a.index, b.index]));
 }
@@ -27,7 +26,7 @@ function getMax3(ar) {
  * @param {*} indices the array containing the indices
  */
 function averageOfElements(ar, indices) {
-  if (!Array.isArray(indices) || !Array.isArray(ar)) return NaN;
+  if (!Array.isArray(indices) || indices.length === 0 || !Array.isArray(ar)) return NaN;
   let sum = 0;
   for (let i = 0; i < indices.length; i++) {
     if (indices[i] < 0 || indices[i] >= ar.length) return NaN;
