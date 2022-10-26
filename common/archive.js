f4a30cb33fc4d86ea12166e60c869224b89c7964
frodeheg <frode.heggelund@gmail.com>
2022-10-26 05:28:40 +0200

    Preliminary file for controlling archive data

---
+++ common/archive.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** ****************************************************************************************************
+ * Archive
+ ** ****************************************************************************************************
+ * monthly (never expire)
+ *   .
+ * daily
+ *   .
+ */
+/* function toLocalTime(homey) {
+  homey.settings.get('archive');
+  const localTime = new Date(homeyTime.toLocaleString('en-US', { timeZone: tz }));
+  return localTime;
+}
+
+module.exports = {
+  toLocalTime,
+};
+*/
