8cedd74b509eb2da559f945a610b4425d930f23c
frodeheg <frode.heggelund@gmail.com>
2022-11-11 21:36:00 +0100

    Fixed a bug in the debug script

--- debug_start.sh
+++ debug_start.sh
@@ -2,6 +2,6 @@
 cp .homeycompose/app.json .homeycompose/app.json.bak
 sed -i 's/"no.sparegris"/"no.sparegris2"/g' .homeycompose/app.json
 sed -i 's/"Sparegris"/"Sparegris 2"/g' .homeycompose/app.json
-cp ./app.json ./app.js.bak
+cp ./app.js ./app.js.bak
 sed -i 's/DEBUG_BEGIN/DEBUG_ACTIVE_BEGIN *\//g' ./app.js ./settings/index.html
 sed -i 's/DEBUG_END/\/* DEBUG_ACTIVE_END/g' ./app.js ./settings/index.html
