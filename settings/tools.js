a8833bb429f9ee5ad5c56e038644586103bfa433
frodeheg <frode.heggelund@gmail.com>
2023-04-24 06:31:42 +0200

    Ticket #173: Made time schedules work on all platforms. iFrames are now handled properly

--- settings/tools.js
+++ settings/tools.js
@@ -40,7 +40,70 @@ function averageOfElements(ar, indices) {
   return (sum / indices.length);
 }
 
+/**
+ * Toggles the visibility of an element
+ */
+function toggle(source, id, type = 'block') {
+  const element = document.getElementById(id);
+
+  const visible = (element.style.display !== '') && (element.style.display !== 'none');
+  if (visible) {
+    element.style.display = 'none';
+    if (source) source.style.cursor = 'zoom-in';
+  } else {
+    element.style.display = type;
+    if (source) source.style.cursor = 'zoom-out';
+  }
+  return false; // For onclick not to follow the link
+}
+
+/**
+ * Local function only used by updateTranslations
+ */
+const translationTable = {}; // Cache
+function __localTranslate(string, languageTable) {
+  let parts;
+  try {
+    parts = string.split('.');
+  } catch (err) {
+    if (string in translationTable) {
+      return this.__(translationTable[string]);
+    }
+    return `Missing: ${string}`;
+  }
+  let ptr = languageTable;
+  for (let i = 0; i < parts.length; i++) {
+    if (parts[i] in ptr) {
+      ptr = ptr[parts[i]];
+    } else if (string in translationTable) {
+      // This is a hack to reverse engineer previously loaded strings that has been overwritten by a translation string
+      // This is only applicable when debugging and changing language
+      return this.__(translationTable[string]);
+    } else {
+      console.log(`requested ${string} not found`);
+      return `Missing: ${string}`;
+    }
+  }
+  // console.log(`requested ${string} ==> ${ptr}`);
+  translationTable[ptr] = string;
+  return ptr;
+}
+
+/**
+ * Updates the translations of all html-elements
+ * This is required because Homey does not update iFrame translations
+ */
+function updateTranslations(translations) {
+  // Refresh all html code
+  const objToTranslate = document.querySelectorAll('[data-i18n]');
+  for (let i = 0; i < objToTranslate.length; i++) {
+    objToTranslate[i].innerHTML = __localTranslate(objToTranslate[i].attributes.getNamedItem('data-i18n').value, translations);
+  }
+}
+
 module.exports = {
   getMax3,
   averageOfElements,
+  toggle,
+  updateTranslations,
 };
