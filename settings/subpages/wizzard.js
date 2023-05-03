e7a6371fa585eff96b95b4d52fc0f07c95e3f931
frodeheg <frode.heggelund@gmail.com>
2023-05-03 04:32:55 +0200

    Ticket #51: Made sure the wizzard position cannot be dragged outside of the iframe top/bottom

--- settings/subpages/wizzard.js
+++ settings/subpages/wizzard.js
@@ -400,7 +400,7 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = `${elmnt.offsetTop - pos2}px`;
+    elmnt.style.top = `${Math.min(Math.max(elmnt.offsetTop - pos2, -20), window.innerHeight - 10)}px`;
     elmnt.style.bottom = '';
     elmnt.style.left = `${elmnt.offsetLeft - pos1}px`;
   }
