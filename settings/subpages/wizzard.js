764c1f05484c3ee511a7f175b5466612b233eafd
frodeheg <frode.heggelund@gmail.com>
2023-05-09 04:05:00 +0200

    Ticket #51: Made sure wizzard hint cannot be dragged outside of the screen.

--- settings/subpages/wizzard.js
+++ settings/subpages/wizzard.js
@@ -434,9 +434,10 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = `${Math.min(Math.max(elmnt.offsetTop - pos2, -20), window.innerHeight - 10)}px`;
+    elmnt.style.top = `${Math.min(Math.max(elmnt.offsetTop - pos2, -20), window.innerHeight - 20)}px`;
     elmnt.style.bottom = '';
-    elmnt.style.left = `${elmnt.offsetLeft - pos1}px`;
+    const wizWidth = parseInt(document.getElementById('wizHint').style.width);
+    elmnt.style.left = `${Math.min(Math.max(elmnt.offsetLeft - pos1, 20 - wizWidth), window.innerWidth - 20)}px`;
   }
 
   function closeDragElement(e) {
