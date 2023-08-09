135b79c79b1b0df53c8ae0140eb4a75911ac51af
frodeheg <frode.heggelund@gmail.com>
2023-08-09 03:45:53 +0200

    Ticket #162: Added icon selector for piggy charge controller

--- drivers/piggy-charger/driver.js
+++ drivers/piggy-charger/driver.js
@@ -13,28 +13,6 @@ class MyDriver extends Driver {
     this.homey.app.updateLog('Piggy Bank Charger has been initialized', 1);
   }
 
-  /**
-   * onPairListDevices is called when a user is adding a device
-   * and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
-  async onPairListDevices() {
-    const randHex = () => Math.floor(Math.random() * 65536).toString(16).padStart(4, '0');
-    const randomId = `${randHex()}-${randHex()}-${randHex()}-${randHex()}`;
-    return [
-      {
-        // name: 'My Device',
-        // icon: '',
-        data: {
-          id: randomId
-        }
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      }
-    ];
-  }
-
 }
 
 module.exports = MyDriver;
