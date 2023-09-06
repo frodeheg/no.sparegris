eaa4bc1e5511a28d025c94b2bc0c47777406e72b
frodeheg <frode.heggelund@gmail.com>
2023-09-06 03:06:44 +0200

    Ticket #162: Added checklist for charger controller and connected onoff button properly.

--- drivers/piggy-charger/device.js
+++ drivers/piggy-charger/device.js
@@ -5,6 +5,21 @@
 const { Device } = require('homey');
 const Textify = require('../../lib/textify');
 
+// Driver Manifest references
+const VALIDATION_SETTINGS = 2;
+const STATUS_GOTAMP = 0;
+const STATUS_GOTWATT = 1;
+const STATUS_GOTBATTERY = 2;
+const STATUS_GOTDISCONNECT = 3;
+const STATUS_GOTCONNECT = 4;
+const STATUS_GOTDONE = 5;
+const STATUS_GOTERROR = 6;
+
+// Default text
+const okText = '[\u001b[32;1m OK \u001b[37m]';
+const errText = '[\u001b[31;1mFAIL\u001b[37m]';
+const progressText = '[\u001b[37;0m....\u001b[37;1m]';
+
 class MyDevice extends Device {
 
   /**
@@ -12,38 +27,54 @@ class MyDevice extends Device {
    */
   async onInit() {
     this.homey.app.updateLog('Piggy Charger has been initialized', 1);
+    this.settingsManifest = this.driver.manifest.settings[VALIDATION_SETTINGS].children;
 
-    // Register maintainance action to validate the device
-    this.registerCapabilityListener('button.validate', async () => {
-      this.log('Validating charger');
+    // Register maintainance action used for validation of the device
+    this.registerCapabilityListener('button.reset', async () => {
+      this.log('Reset charger state in order to re-validate it');
       const settings = this.getSettings();
-      if (settings['GotSignalAmps'] !== 'True') return Promise.reject(this.homey.__('charger.warnings.noSignalAmps'));
-      if (settings['GotSignalWatt'] !== 'True') return Promise.reject(new Error(this.homey.__('charger.warnings.noSignalWatts')));
-      if (settings['GotSignalBattery'] !== 'True') return Promise.reject(new Error(this.homey.__('charger.warnings.noSignalBattery')));
-      if (settings['GotSignalStatusDisconnected'] !== 'True') return Promise.reject(new Error(this.homey.__('charger.warnings.noStatusDisconnected')));
-      if (settings['GotSignalStatusConnected'] !== 'True') return Promise.reject(new Error(this.homey.__('charger.warnings.noStatusConnected')));
-      // if (settings['GotSignalStatusDone'] !== 'True') return Promise.reject(new Error(this.homey.__('charger.warnings.noStatusDone')));
-      // if (settings['GotSignalStatusError'] !== 'True') return Promise.reject(new Error(this.homey.__('charger.warnings.noStatusError')));
+      settings.GotSignalAmps = 'False';
+      settings.GotSignalWatt = 'False';
+      settings.GotSignalBattery = 'False';
+      settings.GotSignalStatusDisconnected = 'False';
+      settings.GotSignalStatusConnected = 'False';
+      settings.GotSignalStatusDone = 'False';
+      settings.GotSignalStatusError = 'False';
+      this.setSettings(settings);
+      this.setCapabilityValue('onoff', false);
+      return Promise.resolve();
+    });
+
+    // Link on-off button with the "controllable-device" setting in piggy
+    this.registerCapabilityListener('onoff', async (newVal) => {
+      const deviceId = this.getId();
+      this.log(`Changing onOff state to ${newVal} for Charger: ${deviceId}`);
+      if (newVal) {
+        // Check if the device can be turned on
+        const settings = this.getSettings();
+        if (settings.GotSignalAmps === 'False'
+          || settings.GotSignalWatt === 'False'
+          || settings.GotSignalBattery === 'False'
+          || settings.GotSignalStatusDisconnected === 'False'
+          || settings.GotSignalStatusConnected === 'False'
+          || settings.GotSignalStatusDone === 'False'
+          || settings.GotSignalStatusError === 'False') {
+          return Promise.reject(new Error(this.homey.__('charger.error.notReady')));
+        }
+      }
+      let __deviceList = this.homey.settings.get('deviceList') || {};
+      if (__deviceList === null || !(deviceId in __deviceList)) {
+        __deviceList = await this.homey.app.createDeviceList();
+      }
+      __deviceList[deviceId].use = newVal;
+      this.homey.app.__deviceList = __deviceList;
+      this.homey.settings.set('deviceList', __deviceList);
       return Promise.resolve();
     });
 
     this.homey.images.createImage()
       .then((image) => {
-        image.setStream(async (stream) => {
-          const dst = new Textify({ width: 500, height: 500, colorType: 2, bgColor: { red: 80, green: 80, blue: 80 }});
-          const okText = '[\u001b[32;1m OK \u001b[37m]';
-          const errText = '[\u001b[31;1mFAIL\u001b[37m]';
-          return dst.loadFile('../drivers/piggy-charger/assets/images/notValid.png')
-            .then(() => dst.setCursorWindow(190, 80, 460, 170))
-            .then(() => dst.setTextColor([255, 128, 128, 255]))
-            .then(() => dst.addText('The device can not be used\nbefore the check-list below\nhas been completed\n'))
-            .then(() => dst.addText('-----------------------------'))
-            .then(() => dst.setCursorWindow(40, 185, 460, 460))
-            .then(() => dst.setTextColor([255, 255, 255, 255]))
-            .then(() => dst.addText(`${errText} Connect xxxx\n`))
-            .then(() => dst.addText(`${okText} device\n`))
-            .then(() => dst.pack().pipe(stream));
-        });
+        image.setStream((stream) => this.refreshImageStream(stream));
         this.setCameraImage('front', 'Help image', image);
       })
       .catch(this.error);
@@ -53,6 +84,77 @@ class MyDevice extends Device {
         this.setCameraImage('help', 'Help image 2', image);
       })
       .catch(this.error);
+  }
+
+  /**
+   * Creates a image that can be sent to the device image stream
+   */
+  async refreshImageStream(stream) {
+    const dst = new Textify({ width: 500, height: 500, colorType: 2, bgColor: { red: 80, green: 80, blue: 80 }});
+
+    this.settings = this.getSettings();
+    return dst.loadFile('../drivers/piggy-charger/assets/images/notValid.png')
+      .then(() => dst.setCursorWindow(190, 80, 460, 170))
+      .then(() => dst.setTextColor([255, 128, 128, 255]))
+      .then(() => dst.addText('The device can not be used\nbefore the check-list below\nhas been completed\n'))
+      .then(() => dst.addText('-----------------------------'))
+      .then(() => dst.setCursorWindow(40, 185, 460, 460))
+      .then(() => dst.setTextColor([255, 255, 255, 255]))
+      .then(() => this.runConnectedTest(dst))
+      .then(() => this.runAmpTest(dst))
+      .then(() => this.runWattTest(dst))
+      .then(() => this.runBatteryTest(dst))
+      .then(() => this.runDisconnectTest(dst))
+      .then(() => this.runTurnedOnTest(dst))
+      .then(() => dst.addText(`${progressText} Press refresh for updates\n`))
+      .catch((err) => dst.addText(`\u001b[35;m${err.message}\n`))
+      .finally(() => dst.addText('\u001b[0m(maintenance action "reset" will start over)\u001b[1m\n'))
+      .then(() => dst.pack().pipe(stream));
+  }
+
+  /**
+   * The test procedure is as follows:
+   * 1) Check that a car is connected (requires the user to have updated the flow)
+   */
+  async runTest(dst, settingId, value) {
+    const text = this.settingsManifest[settingId];
+    if (value !== 'True') {
+      dst.addText(`${errText} ${this.homey.__(text.label)}\n`);
+      return Promise.reject(new Error(`${this.homey.__(text.hint)}\n`));
+    }
+    dst.addText(`${okText} ${this.homey.__(text.label)}\n`);
+    return Promise.resolve();
+  }
+
+  async runTurnedOnTest(dst) {
+    const text = this.homey.__('charger.status.turnedOn');
+    const onOffValue = this.getCapabilityValue('onoff');
+    if (!onOffValue) {
+      dst.addText(`${errText} ${text}\n`);
+      return Promise.reject(new Error(`${this.homey.__('charger.tasks.turnOn')}`));
+    }
+    dst.addText(`${okText} ${text}\n`);
+    return Promise.resolve();
+  }
+
+  async runConnectedTest(dst) {
+    return this.runTest(dst, STATUS_GOTCONNECT, this.settings.GotSignalStatusConnected);
+  }
+
+  async runAmpTest(dst) {
+    return this.runTest(dst, STATUS_GOTAMP, this.settings.GotSignalAmps);
+  }
+
+  async runWattTest(dst) {
+    return this.runTest(dst, STATUS_GOTWATT, this.settings.GotSignalWatt);
+  }
+
+  async runBatteryTest(dst) {
+    return this.runTest(dst, STATUS_GOTBATTERY, this.settings.GotSignalBattery);
+  }
+
+  async runDisconnectTest(dst) {
+    return this.runTest(dst, STATUS_GOTDISCONNECT, this.settings.GotSignalStatusDisconnected);
   }
 
   /**
--- testing/test-textify.js
+++ testing/test-textify.js
@@ -1,28 +1,131 @@
+/* eslint-disable comma-dangle */
+
 'use strict';
 
 const fs = require('fs');
 // const { PNG } = require('pngjs/browser');
 const Textify = require('../lib/textify');
 
-const dst = new Textify({
-  width: 500,
-  height: 500,
-  colorType: 2,
-  filterType: 4,
-  bgColor: { red: 80, green: 80, blue: 80 },
-  textCol: { r: 255, g: 255, b: 255, a: 255 },
-  textBg: { r: 0, g: 0, b: 0, a: 0 }
-});
-
+// Driver Manifest references
+const VALIDATION_SETTINGS = 0;
+const STATUS_GOTAMP = 0;
+const STATUS_GOTWATT = 0;
+const STATUS_GOTBATTERY = 0;
+const STATUS_GOTDISCONNECT = 0;
+const STATUS_GOTCONNECT = 0;
+const STATUS_GOTDONE = 0;
+const STATUS_GOTERROR = 0;
 const okText = '[\u001b[32;1m OK \u001b[37m]';
 const errText = '[\u001b[31;1mFAIL\u001b[37m]';
-dst.loadFile('../drivers/piggy-charger/assets/images/notValid.png')
-  .then(() => dst.setCursorWindow(190, 80, 460, 170))
-  .then(() => dst.setTextColor([255, 128, 128, 255]))
-  .then(() => dst.addText('The device can not be used\nbefore the check-list below\nhas been completed\n'))
-  .then(() => dst.addText('-----------------------------'))
-  .then(() => dst.setCursorWindow(40, 185, 460, 460))
-  .then(() => dst.setTextColor([255, 255, 255, 255]))
-  .then(() => dst.addText(`${errText} Connect xxxx\n`))
-  .then(() => dst.addText(`${okText} device\n`))
-  .then(() => dst.pack().pipe(fs.createWriteStream('out.png')));
+const progressText = '[\u001b[37;0m....\u001b[37;1m]';
+
+class Test {
+
+  constructor() {
+    this.driver = {
+      manifest: {
+        settings: [{
+          children: [
+            {
+              label: { "en": "Amp Usage Received" },
+              hint: { "en": "Please make sure a flow updates this device with how many amps the charger is actually charging with whenever the value changes." }
+            }
+          ]
+        }]
+      }
+    };
+    this.settingsManifest = this.driver.manifest.settings[VALIDATION_SETTINGS].children;
+    this.homey = {
+      __: (param) => { return param.en; }
+    }
+  }
+
+  async run() {
+    const dst = new Textify({
+      width: 500,
+      height: 500,
+      colorType: 2,
+      filterType: 4,
+      bgColor: { red: 80, green: 80, blue: 80 },
+      textCol: { r: 255, g: 255, b: 255, a: 255 },
+      textBg: { r: 0, g: 0, b: 0, a: 0 }
+    });
+    this.settings = {
+      GotSignalStatusConnected: 'True',
+      GotSignalAmps: 'True',
+      GotSignalWatt: 'True',
+      GotSignalBattery: 'True',
+      GotSignalStatusDisconnected: 'True',
+      GotSignalStatusDone: 'False',
+      GotSignalStatusError: 'False'
+    };
+    dst.loadFile('../drivers/piggy-charger/assets/images/notValid.png')
+      // --- START COPY ---
+      .then(() => dst.setCursorWindow(190, 80, 460, 170))
+      .then(() => dst.setTextColor([255, 128, 128, 255]))
+      .then(() => dst.addText('The device can not be used\nbefore the check-list below\nhas been completed\n'))
+      .then(() => dst.addText('-----------------------------'))
+      .then(() => dst.setCursorWindow(40, 185, 460, 460))
+      .then(() => dst.setTextColor([255, 255, 255, 255]))
+      .then(() => this.runTurnedOnTest(dst))
+      .then(() => this.runConnectedTest(dst))
+      .then(() => this.runAmpTest(dst))
+      .then(() => this.runWattTest(dst))
+      .then(() => this.runBatteryTest(dst))
+      .then(() => this.runDisconnectTest(dst))
+      .then(() => dst.addText(`${progressText} Press refresh for updates\n`))
+      .catch((err) => dst.addText(`\u001b[35;m${err.message}\n`))
+      .finally(() => dst.addText('\u001b[0m(maintenance action "reset" will start over)\u001b[1m\n'))
+      // --- END COPY ---
+      .then(() => dst.pack().pipe(fs.createWriteStream('out.png')));
+  }
+
+  /**
+   * The test procedure is as follows:
+   * 1) Check that a car is connected (requires the user to have updated the flow)
+   */
+  async runTest(dst, settingId, value) {
+    const text = this.settingsManifest[settingId];
+    if (value !== 'True') {
+      dst.addText(`${errText} ${this.homey.__(text.label)}\n`);
+      return Promise.reject(new Error(`${this.homey.__(text.hint)}\n`));
+    }
+    dst.addText(`${okText} ${this.homey.__(text.label)}\n`);
+    return Promise.resolve();
+  }
+
+  async runTurnedOnTest(dst) {
+    dst.addText(`${okText} ${'Turned on'}\n`);
+    return Promise.resolve();
+  }
+
+  async runConnectedTest(dst) {
+    return this.runTest(dst, STATUS_GOTCONNECT, this.settings.GotSignalStatusConnected);
+  }
+
+  async runAmpTest(dst) {
+    return this.runTest(dst, STATUS_GOTAMP, this.settings.GotSignalAmps);
+  }
+
+  async runWattTest(dst) {
+    return this.runTest(dst, STATUS_GOTWATT, this.settings.GotSignalWatt);
+  }
+
+  async runBatteryTest(dst) {
+    return this.runTest(dst, STATUS_GOTBATTERY, this.settings.GotSignalBattery);
+  }
+
+  async runDisconnectTest(dst) {
+    return this.runTest(dst, STATUS_GOTDISCONNECT, this.settings.GotSignalStatusDisconnected);
+  }
+
+/*
+const  = 0;
+const  = 0;
+const STATUS_GOTDONE = 0;
+const STATUS_GOTERROR = 0;*/
+
+}
+
+const myTest = new Test();
+myTest.run();
