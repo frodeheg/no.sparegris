587992121b4551664ad469d7ff2ac87ac1d8fba3
frodeheg <frode.heggelund@gmail.com>
2023-09-09 02:21:06 +0200

    Ticket #162: Added Easee to pairing screen + improved layout of icon selection

--- drivers/piggy-charger/driver.js
+++ drivers/piggy-charger/driver.js
@@ -13,6 +13,54 @@ class MyDriver extends Driver {
     this.homey.app.updateLog('Piggy Bank Charger has been initialized', 1);
   }
 
+  /**
+   * onPairListDevices is called when a user is adding a device
+   * and the 'list_devices' view is called.
+   * This should return an array with the data of devices that are available for pairing.
+   */
+  async onPairListDevices() {
+    const randHex = () => Math.floor(Math.random() * 65536).toString(16).padStart(4, '0');
+    const randomId = `${randHex()}-${randHex()}-${randHex()}-${randHex()}`;
+    const devicelist = [];
+
+    // First entry is a new flow based charge controller
+    // There can be many of this, hence a random id
+    const flowDevice = {
+      name: this.homey.__('charger.new.flowBased'),
+      icon: 'flow.svg',
+      data: {
+        id: randomId,
+        easee: false
+      }
+      //   store: {
+      //     address: '127.0.0.1',
+      //   },
+    };
+    devicelist.push(flowDevice);
+
+    // Add all Easee devices
+    const devices = await this.homey.app.createDeviceList();
+    const deviceIds = Object.keys(devices);
+    for (let idx = 0; idx < deviceIds.length; idx++) {
+      const deviceId = deviceIds[idx];
+      const { driverId, name } = devices[deviceId];
+      if (driverId === 'no.easee:charger') {
+        const easeeCharger = {
+          name: `${this.homey.__('charger.new.controlFor')} ${name}`,
+          icon: 'easee.svg',
+          data: {
+            id: deviceId,
+            easee: true
+          }
+        };
+        devicelist.push(easeeCharger);
+      }
+    }
+
+    this.log('onPairListDevices');
+    return devicelist;
+  }
+
 }
 
 module.exports = MyDriver;
